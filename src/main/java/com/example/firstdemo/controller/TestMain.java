a508c469f1eb26706df8019ea51193b874f394ab
Logan <15927171644@163.com>
2019-04-22 17:54:13 +0800

    测试远程提交

---
+++ src/main/java/com/example/firstdemo/controller/TestMain.java
@@ -0,0 +1,10 @@
+package com.example.firstdemo.controller;
+
+/**
+ * 测试函数提交
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("测试提交输出");
+    }
+}
